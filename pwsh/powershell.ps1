fe85bdd58408f17a08222faab84f10129fc78980
kduongthb <20521444@gm.uit.edu.vn>
2023-12-07 01:17:26 +0700

    add various windows configs

---
+++ pwsh/powershell.ps1
@@ -0,0 +1,20 @@
+oh-my-posh init pwsh --config "D:\dotfiles\catppucin.omp.json" | Invoke-Expression
+clear
+
+echo ""
+fastfetch --config "D:\dotfiles\fastfetch\windows"
+echo ""
+
+function sudo {
+Start-Process pwsh -Verb RunAs
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
